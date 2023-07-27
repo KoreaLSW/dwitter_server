0af4cc2e200555aa8e6de8b677e52428c6e5fb7c
sangwoo <sagnwoo0560@gmail.com>
2023-07-27 19:24:09 +0900

    포트번호 변경

--- app.js
+++ app.js
@@ -39,5 +39,5 @@ db.getConnection().then((connection) =>
     console.log(`Server is Started... ${new Date()}`)
 );
 
-const server = app.listen(8080);
+const server = app.listen(3000);
 initSocket(server);
